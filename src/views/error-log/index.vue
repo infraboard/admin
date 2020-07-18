94b6fdbacae08fb03302a40679221ff8d5c4aeab
g7 <123456>
2020-07-18 12:57:17 +0800

    初始化

---
+++ src/views/error-log/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="errPage-container">
+    <!-- $t is vue-i18n global function to translate lang -->
+    <h3>{{ $t('errorLog.tips') }}</h3>
+    <aside>
+      {{ $t('errorLog.description') }}
+      <a target="_blank" class="link-type" href="https://panjiachen.github.io/vue-element-admin-site/guide/advanced/error.html">
+        {{ $t('errorLog.documentation') }}
+      </a>
+    </aside>
+    <a href="#">
+      <img src="https://wpimg.wallstcn.com/360e4842-4db5-42d0-b078-f9a84a825546.gif">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorLog',
+  components: {}
+}
+</script>
+
+<style scoped>
+  .errPage-container {
+    padding: 30px;
+  }
+</style>
