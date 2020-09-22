466fa80f064b9f9f6a184100d266f5d04460bbd2
“yumaojun03” <18108053819@163.com>
2020-09-22 09:51:05 +0800

    调整表单

--- src/views/keyauth/sub-account/detail.vue
+++ src/views/keyauth/sub-account/detail.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="app-container">
     <div class="title-row">
-      <div id="left" class="detail-left">
+      <!-- 左侧box -->
+      <div class="detail-left">
         <el-card class="box-card f12">
           <el-row :gutter="8">
             <el-col :xs="12" :sm="12" :lg="12">
               <span class="title">用户详情</span>
             </el-col>
             <el-col :xs="12" :sm="12" :lg="12">
-              <el-button class="fr" type="text" size="mini" @click="handleUpdate(row)">编辑</el-button>
+              <div v-show="isEdit" class="fr">
+                <el-button type="text" size="mini" @click="save">保存</el-button>
+                <el-button type="text" size="mini" @click="cancel">取消</el-button>
+              </div>
+              <div v-show="!isEdit">
+                <el-button class="fr" type="text" size="mini" @click="handleUpdate()">编辑</el-button>
+              </div>
             </el-col>
           </el-row>
           <el-col class="detail-col" :xs="18" :sm="18" :lg="12">
             <el-row class="attr-row">
               <span class="attr-key">名称</span>
               <span class="attr-value">{{ user.account }}</span>
-            </el-row>
-            <el-row class="attr-row">
-              <span class="attr-key">性别</span>
-              <div class="attr-value">
-                <span v-if="user.gender">{{ user.gender }}</span>
-                <span v-else> 未知 </span>
-              </div>
             </el-row>
             <el-row class="attr-row">
               <span class="attr-key">头像</span>
@@ -31,28 +31,54 @@
               </div>
             </el-row>
             <el-row class="attr-row">
+              <span class="attr-key">性别</span>
+              <div class="attr-value">
+                <div v-show="isEdit">
+                  <el-radio v-model="user.gender" label="1">男</el-radio>
+                  <el-radio v-model="user.gender" label="2">女</el-radio>
+                  <el-radio v-model="user.gender" label="">保密</el-radio>
+                </div>
+                <div v-show="!isEdit">
+                  <span v-if="user.gender">{{ user.gender }}</span>
+                  <span v-else> 保密 </span>
+                </div>
+              </div>
+            </el-row>
+            <el-row class="attr-row">
               <span class="attr-key">昵称</span>
               <div class="attr-value">
-                <span>{{ user.nick_name }}</span>
+                <el-input v-show="isEdit" v-model="form.nick_name" maxlength="20" show-word-limit placeholder="用户昵称" />
+                <span v-show="!isEdit">{{ user.nick_name }}</span>
               </div>
             </el-row>
             <el-row class="attr-row">
               <span class="attr-key">姓名</span>
               <div class="attr-value">
-                <span>{{ user.real_name }}</span>
+                <el-input v-show="isEdit" v-model="form.real_name" maxlength="20" show-word-limit placeholder="真实姓名" />
+                <span v-show="!isEdit">{{ user.real_name }}</span>
               </div>
             </el-row>
             <el-row class="attr-row">
               <span class="attr-key">电话</span>
-              <span class="attr-value">{{ user.mobile }}</span>
+              <div class="attr-value">
+                <el-input v-show="isEdit" v-model="form.mobile" maxlength="20" show-word-limit placeholder="电话号码" />
+                <span v-show="!isEdit">{{ user.mobile }}</span>
+              </div>
             </el-row>
             <el-row class="attr-row">
               <span class="attr-key">邮箱</span>
-              <span class="attr-value">{{ user.email }}</span>
+              <div class="attr-value">
+                <el-input v-show="isEdit" v-model="form.email" maxlength="20" show-word-limit placeholder="邮箱地址" />
+                <span v-show="!isEdit">{{ user.email }}</span>
+              </div>
             </el-row>
             <el-row class="attr-row">
               <span class="attr-key">城市</span>
-              <span class="attr-value">{{ user.province }} {{ user.city }}</span>
+              <div class="attr-value">
+                <el-input v-show="isEdit" v-model="form.province" style="width:48%" maxlength="10" show-word-limit placeholder="省" />
+                <el-input v-show="isEdit" v-model="form.city" style="margin-left:4%;width:48%" maxlength="10" show-word-limit placeholder="省" />
+                <span v-show="!isEdit">{{ user.province }} {{ user.city }}</span>
+              </div>
             </el-row>
           </el-col>
           <el-col :offset="2" class="detail-col" :xs="18" :sm="18" :lg="10">
@@ -87,16 +113,15 @@
           </el-col>
         </el-card>
       </div>
-      <div id="right" class="detail-right">
-        <el-card class="box-card f12" :style="rightStyle">
-          <el-row :gutter="8">
-            <span class="title">快捷操作</span>
-          </el-row>
-          <el-row :gutter="8" style="margin-top:8px;">
-            <el-button style="margin-left:22px;">修改密码</el-button>
-          </el-row>
-        </el-card>
-      </div>
+      <!-- 右侧box -->
+      <el-card class="box-card f12 detail-right" :style="rightStyle">
+        <el-row :gutter="8">
+          <span class="title">快捷操作</span>
+        </el-row>
+        <el-row :gutter="8" style="margin-top:8px;">
+          <el-button style="margin-left:22px;">修改密码</el-button>
+        </el-row>
+      </el-card>
     </div>
 
     <el-card class="box-card" style="margin-top:12px;">
@@ -134,6 +159,7 @@ export default {
   directives: { },
   data() {
     return {
+      isEdit: false,
       activeName: 'first',
       tableKey: 0,
       user: {},
@@ -163,20 +189,7 @@ export default {
   created() {
     this.getSubAccountDetail()
   },
-  mounted() {
-    this.rightStyle.height = this.adjustHeight()
-  },
   methods: {
-    adjustHeight() {
-      var left = document.getElementById('left')
-      var right = document.getElementById('right')
-      if (left && right) {
-        if (left.offsetHeight >= right.offsetHeight) {
-          return left.offsetHeight + 'px'
-        }
-        return right.offsetHeight + 'px'
-      }
-    },
     getSubAccountDetail() {
       this.queryLoading = true
       // 获取用户列表
@@ -194,64 +207,42 @@ export default {
         description: ''
       }
     },
-    handleCreate() {
-      this.dialogFormType = 'create'
-      this.resetForm()
-      this.dialogFormVisible = true
-      this.$nextTick(() => {
-        this.$refs['dataForm'].clearValidate()
-      })
+    handleUpdate() {
+      this.dialogFormType = 'update'
+      this.form = Object.assign({}, this.user) // copy obj
+      this.isEdit = true
     },
-    submit() {
+    save() {
       this.$refs['dataForm'].validate((valid) => {
         if (valid) {
           if (this.dialogFormType === 'create') {
-            // 新建
+          // 新建
             this.createNamespace()
           } else {
-            // 更新
+          // 更新
           }
         }
       })
     },
-    createNamespace() {
-      this.createLoading = true
-      // 创建请求
-      // createNamespace(this.form).then(resp => {
-      //   this.dialogFormVisible = false
-      //   this.roleList.unshift(resp.data)
-      //   this.$notify({
-      //     title: '成功',
-      //     message: '创建成功',
-      //     type: 'success',
-      //     duration: 2000
-      //   })
-      //   this.createLoading = false
-      // }).catch(() => {
-      //   this.createLoading = false
-      // })
-    },
-    handleUpdate(row) {
-      this.dialogFormType = 'update'
-      this.form = Object.assign({}, row) // copy obj
-      this.dialogFormVisible = true
-      this.$nextTick(() => {
-        this.$refs['dataForm'].clearValidate()
-      })
+    cancel() {
+      this.isEdit = false
+      console.log('xxx')
     }
   }
+
 }
 </script>
 
 <style lang="scss" scoped>
 .title-row {
-  display: flex;
+  display: -webkit-box;
 }
 .detail-left {
   width: 74%;
 }
 .detail-right {
   margin-left: 1%;
-  width: 25%
+  width: 25%;
+  border: 1px solid #e6ebf5;
 }
 </style>
