e85286c7b4b2077778dabefac5b15a3f1fec75ce
“yumaojun03” <18108053819@163.com>
2020-10-30 13:57:13 +0800

    补充需要录入的用户信息

--- src/views/keyauth/profile/initial.vue
+++ src/views/keyauth/profile/initial.vue
@@ -11,31 +11,53 @@
       </div>
       <div class="form-content">
         <el-divider />
-        <el-form label-width="80px" :rules="rules" label-position="left" :model="profileForm" style="margin-top:24px;">
-          <el-form-item label="电话" :label-width="formLabelWidth" prop="mobile">
-            <el-input v-model="profileForm.mobile" maxlength="40" show-word-limit />
-            <div class="input-tips">
-              <span>电话号码将用于密码找回,消息通知和多因子认证</span>
-            </div>
-          </el-form-item>
-          <el-form-item label="邮箱" :label-width="formLabelWidth" prop="email">
-            <el-input v-model="profileForm.email" maxlength="60" show-word-limit />
-            <div class="input-tips">
-              <span>用于订阅站内消息</span>
-            </div>
-          </el-form-item>
-          <el-form-item label="性别" :label-width="formLabelWidth" prop="gender">
-            <el-radio v-model="profileForm.gender" label="male">男</el-radio>
-            <el-radio v-model="profileForm.gender" label="female">女</el-radio>
-            <el-radio v-model="profileForm.gender" label="unknown">保密</el-radio>
-          </el-form-item>
+        <div style="margin-top:24px;">
+          <!-- profile form -->
+          <el-form v-if="active === 0" ref="profileFormData" :rules="rules" label-position="left" :model="profileForm">
+            <el-form-item label="姓名" :label-width="formLabelWidth" prop="real_name">
+              <el-input v-model="profileForm.real_name" maxlength="40" show-word-limit />
+              <div class="input-tips">
+                <span>用户真实姓名, 用于界面展示</span>
+              </div>
+            </el-form-item>
+            <el-form-item label="电话" :label-width="formLabelWidth" prop="mobile">
+              <el-input v-model="profileForm.mobile" maxlength="40" show-word-limit />
+              <div class="input-tips">
+                <span>电话号码将用于密码找回,消息通知和多因子认证</span>
+              </div>
+            </el-form-item>
+            <el-form-item label="邮箱" :label-width="formLabelWidth" prop="email">
+              <el-input v-model="profileForm.email" maxlength="60" show-word-limit />
+              <div class="input-tips">
+                <span>用于订阅站内消息</span>
+              </div>
+            </el-form-item>
+            <el-form-item label="性别" :label-width="formLabelWidth" prop="gender">
+              <el-radio v-model="profileForm.gender" label="male">男</el-radio>
+              <el-radio v-model="profileForm.gender" label="female">女</el-radio>
+              <el-radio v-model="profileForm.gender" label="unknown">保密</el-radio>
+            </el-form-item>
+            <el-form-item label="语言" :label-width="formLabelWidth" prop="language">
+              <el-select v-model="profileForm.language" placeholder="请选择">
+                <el-option
+                  v-for="item in languageOptions"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                />
+              </el-select>
+            </el-form-item>
+          </el-form>
+          <!-- department -->
+        </div>
+
+        <div>
           <div>
             <el-button :disabled="active === 0" @click="previous">上一步</el-button>
             <el-button v-if="active < 2" type="primary" @click="next">下一步</el-button>
             <el-button v-if="active === 2" type="primary" style="width:68px;" @click="next">完 成</el-button>
           </div>
-        </el-form>
-
+        </div>
       </div>
 
     </div>
@@ -45,28 +67,50 @@
 <script>
 import Tips from '@/components/Tips'
 
+const languageOptions = [
+  { label: '中文', value: 'zh' },
+  { label: 'English', value: 'en' }
+]
+
 export default {
   name: 'DashboardEditor',
   components: { Tips },
   data() {
     return {
+      languageOptions,
       active: 0,
       formLabelWidth: '50px',
       profileForm: {
         mobile: '',
         email: '',
-        gender: 'unknown'
+        gender: 'unknown',
+        language: 'zh'
       },
       departForm: {},
       rules: {
+        real_name: [{ required: true, message: '请输入姓名', trigger: 'change' }],
         mobile: [{ required: true, message: '请输入手机号码', trigger: 'change' }],
-        email: [{ required: true, message: '请输入邮箱地址', trigger: 'blur' }]
+        email: [{ required: true, message: '请输入邮箱地址', trigger: 'change' }]
       }
     }
   },
   methods: {
     next() {
-      if (this.active++ > 2) this.active = 2
+      switch (this.active) {
+        case 0:
+          this.$refs['profileFormData'].validate((valid) => {
+            if (valid) {
+              this.active++
+            }
+          })
+          break
+        case 1:
+          console.log('1')
+          break
+        case 2:
+          console.log('2')
+          break
+      }
     },
     previous() {
       if (this.active-- < 0) this.active = 0
