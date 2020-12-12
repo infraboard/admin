f0316b10419223c4c5eafdbe83de5e3b37260246
“yumaojun03” <18108053819@163.com>
2020-12-12 16:10:07 +0800

    添加逻辑

--- src/permission.js
+++ src/permission.js
@@ -36,7 +36,6 @@ router.beforeEach(async(to, from, next) => {
           // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
           const { roles } = await store.dispatch('user/getInfo')
 
-          console.log('add async route')
           // generate accessible routes map based on roles
           const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
 
